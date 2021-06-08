0ccff87776f2e54848a958ff6fda93965789d5a6
chester0520 <clemo0520@gmail.com>
2021-06-08 11:35:18 +0800

    123

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+#home
+  img(:src='require("../assets/0104.jpg")')
+  br
+  | 嗨！歡迎加入國立科技高中。 參加社團不僅可以豐富自己 的人生、寬闊自己的視野， 也能砥礪技能、磨練人際、培養第二專長。 選擇一項您喜歡的社團，積極的參與和投入， 您會獲得一陣陣的驚喜 ！
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
